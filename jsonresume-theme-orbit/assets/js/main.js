67f2f74459624eb55638bbdc64dff08a5ddfaf36
XuluWarrior <github@metaliving.plus.com>
2017-12-01 14:06:39 +0000

    First commit

---
+++ jsonresume-theme-orbit/assets/js/main.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function($) {
+
+
+    /*======= Skillset *=======*/
+    
+    
+    $('.level-bar-inner').css('width', '0');
+    
+    $('.level-bar-inner').each(function() {
+
+        var itemWidth = $(this).data('level');
+
+        $(this).animate({
+            width: itemWidth
+        }, 800);
+
+    });
+
+    
+
+});
---
+++ jsonresume-theme-orbit/index.js
@@ -0,0 +1,75 @@
+var fs = require("fs");
+var path = require('path');
+var Handlebars = require("handlebars");
+
+
+Handlebars.registerHelper('displayUrl', function(str) {
+	return str.replace(/https?:\/\//, "");
+});
+
+Handlebars.registerHelper('toLowerCase', function(str) {
+	return str.toLowerCase();
+});
+
+Handlebars.registerHelper('year', function(str) {
+	if (str) {
+		var d = new Date(str);
+		return d.getFullYear();
+	} else {
+		return "Present"
+	}
+});
+
+Handlebars.registerHelper('award', function(str) {
+	switch (str.toLowerCase()) {
+		case "bachelor":
+		case "master":
+			return str + "s";
+		default:
+			return str;
+	}
+});
+
+Handlebars.registerHelper('skillLevel', function(str) {
+	switch (str.toLowerCase()) {
+		case "beginner":
+			return "25%";
+		case "intermediate":
+			return "50%";
+		case "advanced":
+			return "75%";
+		case "master":
+			return "100%";
+		default:
+			return parseInt(str) + "%"
+	}
+});
+
+function render(resume) {
+	var css = fs.readFileSync(__dirname + "/assets/css/styles.css", "utf-8");
+	var js = fs.readFileSync(__dirname + "/assets/js/main.js", "utf-8");
+	var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
+	var partialsDir = path.join(__dirname, 'partials');
+	var filenames = fs.readdirSync(partialsDir);
+
+	filenames.forEach(function (filename) {
+	  var matches = /^([^.]+).hbs$/.exec(filename);
+	  if (!matches) {
+	    return;
+	  }
+	  var name = matches[1];
+	  var filepath = path.join(partialsDir, filename);
+	  var template = fs.readFileSync(filepath, 'utf8');
+
+	  Handlebars.registerPartial(name, template);
+	});
+	return Handlebars.compile(tpl)({
+		css: css,
+		js: js,
+		resume: resume
+	});
+}
+
+module.exports = {
+	render: render
+};
