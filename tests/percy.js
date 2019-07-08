74cd6fd0dab68d446f0a4e740ae4851de61c19ff
XuluWarrior <github@metaliving.plus.com>
2019-07-08 15:04:12 +0100

    Wait for animation to finish before taking Percy snapshot Use @percy/script instead of @percy/agent

---
+++ tests/percy.js
@@ -0,0 +1,7 @@
+const PercyScript = require('@percy/script');
+
+PercyScript.run(async (page, percySnapshot) => {
+    await page.goto('http://localhost:5001/index.html');
+    await page.waitFor(1000); // animation to finish
+    await percySnapshot('/example-resume.html');
+});
