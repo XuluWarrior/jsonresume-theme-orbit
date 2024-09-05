12d517ac805e61beafb198e912219874cf970a81
XuluWarrior <github@metaliving.plus.com>
2024-09-05 16:07:27 +0100

    Remove use of var

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var fs = require("fs");
-var path = require('path');
-var Handlebars = require("handlebars");
-var utils = require('handlebars-utils');
-var marked = require('marked').marked;
+const fs = require("fs");
+const path = require('path');
+const Handlebars = require("handlebars");
+const utils = require('handlebars-utils');
+const marked = require('marked').marked;
 
 Handlebars.registerHelper('markdown', function(str, locals, options) {
 	if (typeof str !== 'string') {
@@ -16,13 +16,13 @@ Handlebars.registerHelper('markdown', function(str, locals, options) {
 		locals = {};
 	}
 
-	var ctx = utils.context(this, locals, options);
-	var val = utils.value(str, ctx, options);
+	const ctx = utils.context(this, locals, options);
+	const val = utils.value(str, ctx, options);
 
-	var markup = marked(val);
+	const markup = marked(val);
 
 	// If we end up with a string wrapped in one <p> block, remove it so we don't create a new text block
-	var startEndMatch = markup.match(/^<p>(.*)<\/p>\n$/);
+	const startEndMatch = markup.match(/^<p>(.*)<\/p>\n$/);
 	return startEndMatch && startEndMatch[1].indexOf("<p>") === -1 ?
 		startEndMatch[1] :
 		markup;
@@ -38,7 +38,7 @@ Handlebars.registerHelper('toLowerCase', function(str) {
 
 Handlebars.registerHelper('year', function(str) {
 	if (str) {
-		var d = new Date(str);
+		const d = new Date(str);
 		return d.getFullYear();
 	} else {
 		return "Present"
@@ -88,8 +88,8 @@ function fixResume(resume) {
 
 function fixAllEntries(entries) {
 	if (entries) {
-		for (var i=0; i < entries.length; i++) {
-			var entry = entries[i];
+		for (let i=0; i < entries.length; i++) {
+			const entry = entries[i];
 			if (entry.website) {
 				entry.url = entry.website;
 				delete entry.website;
@@ -101,8 +101,8 @@ function fixAllEntries(entries) {
 // work.company has been renamed as work.name in v1.0.0
 function fixWork(work) {
 	if (work) {
-		for (var i=0; i < work.length; i++) {
-			var entry = work[i];
+		for (let i=0; i < work.length; i++) {
+			const entry = work[i];
 			if (entry.company) {
 				entry.name = entry.company;
 				delete entry.website;
@@ -113,23 +113,23 @@ function fixWork(work) {
 }
 
 function render(resume) {
-	var css = fs.readFileSync(__dirname + "/assets/css/styles.css", "utf-8");
-	var js = fs.readFileSync(__dirname + "/assets/js/main.js", "utf-8");
-	var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
+	const css = fs.readFileSync(__dirname + "/assets/css/styles.css", "utf-8");
+	const js = fs.readFileSync(__dirname + "/assets/js/main.js", "utf-8");
+	const tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
 
 	fixResume(resume);
 
-	var partialsDir = path.join(__dirname, 'partials');
-	var filenames = fs.readdirSync(partialsDir);
+	const partialsDir = path.join(__dirname, 'partials');
+	const filenames = fs.readdirSync(partialsDir);
 
 	filenames.forEach(function (filename) {
-	  var matches = /^([^.]+).hbs$/.exec(filename);
+	  const matches = /^([^.]+).hbs$/.exec(filename);
 	  if (!matches) {
 	    return;
 	  }
-	  var name = matches[1];
-	  var filepath = path.join(partialsDir, filename);
-	  var template = fs.readFileSync(filepath, 'utf8');
+	  const name = matches[1];
+	  const filepath = path.join(partialsDir, filename);
+	  const template = fs.readFileSync(filepath, 'utf8');
 
 	  Handlebars.registerPartial(name, template);
 	});
